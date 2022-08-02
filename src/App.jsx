a3172ef237e2f8bccdea335eabb1c64ed21a395e
Guy Lillico <guylillico@gmail.com>
2022-08-02 18:58:46 +0800

    Displaying table for total requests per IP address

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import FileUpload from "./components/FileUpload/FileUpload"
 import FeaturedMetricsWrapper from "./components/FeaturedMetricsWrapper/FeaturedMetricsWrapper"
 import FeaturedMetric from "./components/FeaturedMetric/FeaturedMetric"
-
+import MetricsTable from "./components/MetricsTable/MetricsTable"
 import { parseLogData } from "./util/logParser"
 import "./App.css"
 
@@ -32,33 +32,42 @@ const App = () => {
       </header>
       <p>Select a log file to view metrics</p>
       <FileUpload onLogFileChange={handleUploadFileChange} />
-
-      {error && <p>{error}</p>}
-      {!error && Object.keys(uniqueIps).length > 0 && (
-        <FeaturedMetricsWrapper>
-          <FeaturedMetric title="Total unique IP addresses">
-            <span>{totalUniqueIps}</span>
-          </FeaturedMetric>
-          <FeaturedMetric title="Top 3 most active IP addresses">
-            <ol>
-              {Object.keys(uniqueIps)
-                .splice(0, 3)
-                .map((key) => {
-                  return <li key={key}>{uniqueIps[key]?.ip}</li>
-                })}
-            </ol>
-          </FeaturedMetric>
-          <FeaturedMetric title="Top 3 most visited URLs">
-            <ol>
-              {Object.keys(uniqueUrls)
-                .splice(0, 3)
-                .map((key) => {
-                  return <li key={key}>{uniqueUrls[key]?.url}</li>
-                })}
-            </ol>
-          </FeaturedMetric>
-        </FeaturedMetricsWrapper>
-      )}
+      <div className="container">
+        {error && <p>{error}</p>}
+        {!error && Object.keys(uniqueIps).length > 0 && (
+          <React.Fragment>
+            <FeaturedMetricsWrapper>
+              <FeaturedMetric title="Total unique IP addresses">
+                <span>{totalUniqueIps}</span>
+              </FeaturedMetric>
+              <FeaturedMetric title="Top 3 most active IP addresses">
+                <ol>
+                  {Object.keys(uniqueIps)
+                    .splice(0, 3)
+                    .map((key) => {
+                      return <li key={key}>{uniqueIps[key]?.ip}</li>
+                    })}
+                </ol>
+              </FeaturedMetric>
+              <FeaturedMetric title="Top 3 most visited URLs">
+                <ol>
+                  {Object.keys(uniqueUrls)
+                    .splice(0, 3)
+                    .map((key) => {
+                      return <li key={key}>{uniqueUrls[key]?.url}</li>
+                    })}
+                </ol>
+              </FeaturedMetric>
+            </FeaturedMetricsWrapper>
+            <MetricsTable
+              title="Requests per IP Address"
+              metric="ip"
+              header={["IP", "No. of Requests"]}
+              body={uniqueIps}
+            />
+          </React.Fragment>
+        )}
+      </div>
     </div>
   )
 }
