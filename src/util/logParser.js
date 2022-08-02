5cb04195ba263873c96b2ab007db72ad6745b80a
Guy Lillico <guylillico@gmail.com>
2022-08-02 17:56:51 +0800

    creating a new object with each unique IP and count

--- src/util/logParser.js
+++ src/util/logParser.js
@@ -1,5 +1,28 @@
+export const generateUniqueIpsObj = (dataArray) => {
+  const uniqueIpsObj = {}
+  const ipRegex = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(.|$)){4}\b/
+
+  dataArray.forEach((item) => {
+    console.log(item)
+    const matchIp = item.match(ipRegex)
+    const ip = matchIp && matchIp.length ? matchIp[0] : null
+
+    if (!ip || ip === "") return
+    if (uniqueIpsObj[ip]) {
+      uniqueIpsObj[ip]["count"]++
+    } else {
+      uniqueIpsObj[ip] = {
+        count: 1,
+        ip: ip,
+      }
+    }
+  })
+
+  return uniqueIpsObj
+}
+
 export const parseLogData = (file) => {
   const fileData = file.split("\n")
-
-  console.log(fileData)
+  const uniqueIps = generateUniqueIpsObj(fileData)
+  console.log(uniqueIps)
 }
